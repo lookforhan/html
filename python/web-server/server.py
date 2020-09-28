1a91d2b6b96e27cbd3c2a047122200e20c86c190
lookforhan <767122454@qq.com>
2020-09-28 20:50:06 +0800

    update server.py

--- python/web-server/server.py
+++ python/web-server/server.py
@@ -6,22 +6,23 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     # 页面模板
     Page = '''\
-            <html>
-            <body>
-            <p>Hello,web!</p>
-            </body>
-            </html>
-            '''
-    # 处理一共GET请求
+        <html>
+        <body>
+        <p>Hello, web!</p>
+        </body>
+        </html>
+    '''
+
+    # 处理一个GET请求
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-Type","text/html")
-        self.send_header("Content-Length",str(len(self.Page)))
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
         self.end_headers()
         self.wfile.write(self.Page.encode('utf-8'))
 
-        if __name__ == '__main__':
-            serverAddress = ('',8080)
-            server = HTTPServer(serverAddress,RequestHandler)
-            server.serve_forever()
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
 
